63cb2e6306593b6062a31ef5ef43b83cba064031
Jared Thirsk <ja@thirsk.ca>
2017-03-15 22:36:04 -0600

    CONVERSION: project.json to csproj - works

---
+++ src/LionFire.Runtime.Extensions/Net/Messages/MNetEvent.cs
@@ -0,0 +1,34 @@
+﻿using LionFire.Messaging;
+using LionFire.Net.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LionFire.Net
+{
+    public class NetEvents
+    {
+        public static void OnFail(string uri, object request = null)
+        {
+            new MInternetFailure { Url = uri, Request = request }.Publish();
+        }
+        public static void OnSuccess(string uri, object request = null)
+        {
+            new MInternetSuccess { Url = uri, Request = request }.Publish();
+        }
+    }
+}
+namespace LionFire.Net.Messages
+{
+    public class MNetEvent : IMessage
+    {
+        public string Url { get; set; }
+        public object Request { get; set; }
+    }
+    public class MInternetFailure : MNetEvent
+    {
+
+
+    }
+    public class MInternetSuccess : MNetEvent { }
+}
