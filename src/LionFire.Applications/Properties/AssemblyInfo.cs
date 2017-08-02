80a00814aa9dccd42a474403a319164a83ffddae
Jared Thirsk <ja@thirsk.ca>
2017-08-02 17:41:45 -0600

    attempted fix for ci

--- src/LionFire.Applications/Properties/AssemblyInfo.cs
+++ src/LionFire.Applications/Properties/AssemblyInfo.cs
@@ -2,10 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LionFire Games")]
 [assembly: AssemblyProduct("LionFire.Applications")]
 [assembly: AssemblyTrademark("")]
