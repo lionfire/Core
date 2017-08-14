ba3fa016d2f9a6c33066a9e5ce556138412fd89b
Jared Thirsk <ja@thirsk.ca>
2017-08-14 16:30:21 -0600

    add enter/leave tests

--- src/LionFire.StateMachines.Class.Tests.Model/GeneratedExecutable.cs
+++ src/LionFire.StateMachines.Class.Tests.Model/GeneratedExecutable.cs
@@ -12,12 +12,8 @@ namespace LionFire.StateMachines.Class.Tests
     //, ExecutionTransition.Initialize | ExecutionTransition.Start | ExecutionTransition.Finish | ExecutionTransition.CleanUp
     public partial class GeneratedExecutable
     {
-        public void AfterUninitialized()
-        {
-            Log("AfterUninitialized");
-        }
 
-        public bool InitializePrereq { get; set; }
+        public bool InitializePrereq { get; set; } = true;
         public int CanInitializeCount { get; set; }
         public bool CanInitialize
         {
@@ -33,6 +29,42 @@ namespace LionFire.StateMachines.Class.Tests
         {
             Log("OnInitialize");
         }
+
+        #region Leave Uninitialized
+
+        public bool LeaveUninitializedPrereq { get; set; } = true;
+        public int CanLeaveUninitializedCount { get; set; }
+        public bool CanLeaveUninitialized
+        {
+            get
+            {
+                CanLeaveUninitializedCount++;
+                return LeaveUninitializedPrereq;
+            }
+        }
+
+        public void AfterUninitialized()
+        {
+            Log("AfterUninitialized");
+        }
+
+        #endregion
+
+        #region CanReady
+
+        public bool ReadyPrereq { get; set; } = true;
+        public int CanReadyCount { get; set; }
+        public bool CanReady
+        {
+            get
+            {
+                CanReadyCount++;
+                return ReadyPrereq;
+            }
+        }
+        
+        #endregion
+
         public void OnReady()
         {
             Log("OnReady");
--- src/LionFire.StateMachines.Class.Tests/GeneratedExecutableTests.cs
+++ src/LionFire.StateMachines.Class.Tests/GeneratedExecutableTests.cs
@@ -38,13 +38,98 @@ namespace LionFire.StateMachines.Class.Tests
 
         #endregion
 
+        #region CanEnter
+
+        public void EnterPrereq()
+        {
+            var te = new GeneratedExecutable();
+            te.Initialize();
+            Assert.Equal(1, te.CanReadyCount);
+            Assert.Equal(ExecutionState.Ready, te.StateMachine.CurrentState);
+        }
+
+        [Fact]
+        public void EnterPrereqEx()
+        {
+            var te = new GeneratedExecutable();
+            te.ReadyPrereq = false;
+
+            Exception ex = Assert.Throws<CannotChangeStateException>(() => te.Initialize());
+
+            Assert.Equal(ExecutionState.Uninitialized, te.StateMachine.CurrentState);
+        }
+
+        [Fact]
+        public void TryEnterTrue()
+        {
+            var te = new GeneratedExecutable();
+
+            Assert.Equal(true, te.StateMachine.TryChangeState(ExecutionTransition.Initialize));
+
+            Assert.Equal(ExecutionState.Ready, te.StateMachine.CurrentState);
+        }
+
+        [Fact]
+        public void TryEnterFalse()
+        {
+            var te = new GeneratedExecutable();
+            te.ReadyPrereq = false;
+
+            Assert.Equal(false, te.StateMachine.TryChangeState(ExecutionTransition.Initialize));
+
+            Assert.Equal(ExecutionState.Uninitialized, te.StateMachine.CurrentState);
+        }
+
+        #endregion
+        #region CanLeave
+
+        public void LeavePrereq()
+        {
+            var te = new GeneratedExecutable();
+            te.Initialize();
+            Assert.Equal(1, te.CanLeaveUninitializedCount);
+            Assert.Equal(ExecutionState.Ready, te.StateMachine.CurrentState);
+        }
+
+        [Fact]
+        public void LeavePrereqEx()
+        {
+            var te = new GeneratedExecutable();
+            te.LeaveUninitializedPrereq = false;
+
+            Exception ex = Assert.Throws<CannotChangeStateException>(() => te.Initialize());
+
+            Assert.Equal(ExecutionState.Uninitialized, te.StateMachine.CurrentState);
+        }
+
+        [Fact]
+        public void TryLeaveTrue()
+        {
+            var te = new GeneratedExecutable();
+
+            Assert.Equal(true, te.StateMachine.TryChangeState(ExecutionTransition.Initialize));
+
+            Assert.Equal(ExecutionState.Ready, te.StateMachine.CurrentState);
+        }
+
+        [Fact]
+        public void TryLeaveFalse()
+        {
+            var te = new GeneratedExecutable();
+            te.LeaveUninitializedPrereq = false;
+
+            Assert.Equal(false, te.StateMachine.TryChangeState(ExecutionTransition.Initialize));
+
+            Assert.Equal(ExecutionState.Uninitialized, te.StateMachine.CurrentState);
+        }
+
+        #endregion
         #region CanTransition
 
         [Fact]
         public void TransitionPrereq()
         {
             var te = new GeneratedExecutable();
-            te.InitializePrereq = true;
             te.Initialize();
             Assert.Equal(1, te.CanInitializeCount);
             Assert.Equal(ExecutionState.Ready, te.StateMachine.CurrentState);
@@ -65,11 +150,10 @@ namespace LionFire.StateMachines.Class.Tests
         public void TryTransitionTrue()
         {
             var te = new GeneratedExecutable();
-            te.InitializePrereq = true;
 
-            Assert.Equal(te.StateMachine.TryChangeState(ExecutionTransition.Initialize), true);
+            Assert.Equal(true, te.StateMachine.TryChangeState(ExecutionTransition.Initialize));
 
-            Assert.Equal(te.StateMachine.CurrentState, ExecutionState.Ready);
+            Assert.Equal(ExecutionState.Ready, te.StateMachine.CurrentState);
         }
 
         [Fact]
@@ -90,7 +174,6 @@ namespace LionFire.StateMachines.Class.Tests
         {
             var te = new GeneratedExecutable();
             Assert.Equal(te.StateMachine.CurrentState, ExecutionState.Uninitialized);
-            te.InitializePrereq = true;
             te.Initialize();
             Assert.Equal(te.StateMachine.CurrentState, ExecutionState.Ready);
             te.Start();
@@ -107,16 +190,15 @@ namespace LionFire.StateMachines.Class.Tests
             var te = new GeneratedExecutable();
             Func<string> next = () => te.LogMessageQueue.Dequeue();
 
-            te.InitializePrereq = true;
             te.Initialize();
 
             Assert.Equal(next(), "AfterUninitialized");
-            Assert.Equal(te.LogMessageQueue.Dequeue(), "OnInitialize");
-            Assert.Equal(te.LogMessageQueue.Dequeue(), "OnReady");
+            Assert.Equal(next(), "OnInitialize");
+            Assert.Equal(next(), "OnReady");
 
             te.Start();
 
-            //Assert.Equal(te.LogMessageQueue.Dequeue(), "OnStart");
+            //Assert.Equal(next(), "OnStart");
             //Assert.Equal(te.LogMessageQueue.Dequeue(), "AfterReady");
             te.Complete();
             //Assert.Equal(te.LogMessageQueue.Dequeue(), "AfterComplete");
