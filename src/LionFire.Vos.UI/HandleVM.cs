0d15f513055b8f3ac43d69a3de3f81e7dc908f8a
Jared Thirsk <ja+commits@thirsk.ca>
2019-11-07 18:33:53 -0700

    Initial

---
+++ src/LionFire.Vos.UI/HandleVM.cs
@@ -0,0 +1,18 @@
+﻿using LionFire.Persistence;
+
+namespace LionFire.Vos.UI
+{
+    public class HandleVM<T>
+    {
+        protected object Handle { get; }
+        protected virtual void OnHandleChanged(object value)
+        {
+            if(value is INotifyPersists<T> np)
+            {
+                np.PersistenceStateChanged += OnPersistenceStateChanged;
+            }
+        }
+
+        protected virtual void OnPersistenceStateChanged(PersistenceEvent<T> obj) { }
+    }
+}
---
+++ src/LionFire.Vos.UI/ReadHandleVM.cs
@@ -0,0 +1,38 @@
+﻿using LionFire.Persistence;
+using System;
+using System.ComponentModel;
+
+namespace LionFire.Vos.UI
+{
+
+    public class ReadHandleVM<T> : HandleVM<T>, INotifyPropertyChanged
+    {
+
+        #region ReadHandle
+
+        [SetOnce]
+        public IReadHandle<T> ReadHandle
+        {
+            get => readHandle;
+            set
+            {
+                if (readHandle == value) return;
+                if (readHandle != default) throw new AlreadySetException();
+                readHandle = value;
+                OnHandleChanged(value);
+            }
+        }
+        private IReadHandle<T> readHandle;
+
+        #endregion
+
+        #region INotifyPropertyChanged Implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        #endregion
+        
+    }
+}
