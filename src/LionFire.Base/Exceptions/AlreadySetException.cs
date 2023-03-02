6795848eff97182c39b55ed6bf915e37d93e8d19
Jared Thirsk <ja@thirsk.ca>
2023-03-01 21:27:03 -0700

    Added a couple exception classes to LionFire.Base.

---
+++ src/LionFire.Base/Exceptions/AlreadySetException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LionFire;
+
+[Serializable]
+public class AlreadySetException : AlreadyException
+{
+    public const string DefaultMessage = "Property can only be set once";
+
+    public AlreadySetException() : base(DefaultMessage) { }
+    public AlreadySetException(string message) : base(message) { }
+    public AlreadySetException(string message, Exception inner) : base(message, inner) { }
+    protected AlreadySetException(
+      System.Runtime.Serialization.SerializationInfo info,
+      System.Runtime.Serialization.StreamingContext context)
+        : base(info, context) { }
+}
