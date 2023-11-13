b4a5cd9368fd8473c23345ad05bd3713e9d80453
Jared Thirsk <ja+commits@thirsk.ca>
2023-11-13 00:24:30 -0700

    use dotnet's HostDefaults.ApplicationKey "applicationName"

--- src/LionFire.Applications/AppInfo/Configuration/AppConfigurationKeys.cs
+++ src/LionFire.Applications/AppInfo/Configuration/AppConfigurationKeys.cs
@@ -1,10 +1,12 @@
 ﻿
+using Microsoft.Extensions.Hosting;
+
 namespace LionFire.Configuration;
 
 public static class AppConfigurationKeys
 {
     public const string OrgName = "OrgName";
-    public const string AppName = "AppName"; // REVIEW - See also: HostDefaults.ApplicationKey
+    public static string AppName => HostDefaults.ApplicationKey; // "applicationName"
     public const string DataDirName = "DataDirName"; // REVIEW - See also: HostDefaults.ContentRootKey
     public const string AppVersion = "AppVersion";
 }
