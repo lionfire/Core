8206b9b3e71b12cd1c00d8faf858d94304e634a9
Jared Thirsk <ja+commits@thirsk.ca>
2023-11-17 07:37:54 -0700

    nuget upgrader: almost working

--- src/LionFire.Multisolution.Host/Program.cs
+++ src/LionFire.Multisolution.Host/Program.cs
@@ -3,6 +3,7 @@ using LionFire.MultiSolution.Host.Components;
 using LionFire.MultiSolution.Host.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.FluentUI.AspNetCore.Components;
+using System.ComponentModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,9 +13,11 @@ builder.Services.AddFluentUIComponents();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddDbContext<MultiSolutionContext>(options => options.UseFileContextDatabase());
+builder.Services.AddDbContext<MultiSolutionContext>();
+//builder.Services.AddDbContext<MultiSolutionContext>(options => options.UseFileContextDatabase());
 builder.Services.AddSingleton<DocumentService>();
 builder.Services.Configure<DocumentsOptions>(builder.Configuration.GetSection("Documents"));
+builder.Services.AddHostedService<X>();
 
 var app = builder.Build();
 
@@ -36,5 +39,20 @@ public class MultiSolutionContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         //optionsBuilder.UseFileContextDatabase();
+    }
+}
+
+public class X : BackgroundService
+{
+    public X(DocumentService documentService)
+    {
+        DocumentService = documentService;
+    }
+
+    public DocumentService DocumentService { get; }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        return Task.CompletedTask;   
     }
 }
