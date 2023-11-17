20a5213fc418733a8f7323910b048420a2fe1301
Jared Thirsk <ja+commits@thirsk.ca>
2023-11-17 08:47:35 -0700

    remove obsolete serialization formatter ctor

--- src/LionFire.Utility.Legacy/Exceptions/LionFireException.cs
+++ src/LionFire.Utility.Legacy/Exceptions/LionFireException.cs
@@ -11,9 +11,6 @@ namespace LionFire
         public LionFireException() { }
         public LionFireException(string message) : base(message) { }
         public LionFireException(string message, Exception inner) : base(message, inner) { }
-        protected LionFireException(
-          System.Runtime.Serialization.SerializationInfo info,
-          System.Runtime.Serialization.StreamingContext context)
-            : base(info, context) { }
+
     }
 }
