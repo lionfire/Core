d776a7818b1c285f672dadcdeb37599230335c12
Jared Thirsk <ja+commits@thirsk.ca>
2023-11-28 14:01:01 -0700

    stride gfx operational

--- src/LionFire.Hosting.Stride/StrideGameService.cs
+++ src/LionFire.Hosting.Stride/StrideGameService.cs
@@ -1,6 +1,7 @@
 ﻿using LionFire.Stride3D;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Stride.Core.Diagnostics;
 using Stride.Engine;
 using Stride.Games;
 using Stride.Physics;
@@ -12,21 +13,125 @@ using System.Threading.Tasks;
 namespace LionFire.Stride3D
 {
 
-    public static class GameExtensions
+    public static class GameX
     {
         public static Simulation Simulation(this Game game) => game.SceneSystem.SceneInstance.GetProcessor<PhysicsProcessor>()?.Simulation;
     }
 
     public class StrideGameService : IHostedService
     {
-        Task runTask;
+        #region Dependencies
+
+        public IHostApplicationLifetime HostApplicationLifetime { get; }
+        public ILogger<StrideGameService> Logger { get; }
+
+        #endregion
+
+        #region Lifecycle
+
+        public StrideGameService(IHostApplicationLifetime hostApplicationLifetime, ILogger<StrideGameService> logger)
+        {
+            HostApplicationLifetime = hostApplicationLifetime;
+            Logger = logger;
+            GlobalLogger.GlobalMessageLogged += GlobalLogger_GlobalMessageLogged;
+
+        }
+
+        #endregion
+
+        #region State
 
         public Game Game => game;
         private Game game;
 
-        public Simulation Simulation => game.SceneSystem.SceneInstance.GetProcessor<PhysicsProcessor>()?.Simulation;
+        Task strideGameRunTask;
+
+        #region Derived
+
+        public Simulation Simulation => game.Simulation(); // game.SceneSystem.SceneInstance.GetProcessor<PhysicsProcessor>()?.Simulation;
+
+        #endregion
+
+        #endregion
+
+        #region IHostedService
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            strideGameRunTask = Task.Run(() =>
+            {
+                using (game = new Game())
+                {
+                    game.ConsoleLogLevel = LogMessageType.Debug;
+                    game.ConsoleLogMode = ConsoleLogMode.Always;
+                    Task.Run(async () =>
+                    {
+                        while (game?.Window == null)
+                        {
+                            Logger.LogDebug("Waiting for Game.Window to appear...");
+                            await Task.Delay(200);
+                        }
+                        Logger.LogDebug("Waiting for Game window to appear...done.");
+                        game.Window.Closing += Window_Closing;
+                    });
+                    game.Run();
+                }
+            });
+
+            strideGameRunTask.ContinueWith(_ => OnGameFinished());
+            return Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            game.Exit();
+            while (game.IsRunning)
+            {
+                await Task.Delay(100).ConfigureAwait(false);
+            }
+        }
+
+        #endregion
+
+        #region Handlers
+
+        void OnGameFinished()
+        {
+            Logger.LogInformation("Stride Game finished.");
+        }
+
+        private void GlobalLogger_GlobalMessageLogged(ILogMessage obj)
+        {
+            Logger.LogInformation("[stride] [module: {module}] {message}", obj.Module, obj.Text);
+        }
+
+        private void Window_Closing(object sender, EventArgs e)
+        {
+            Logger.LogInformation($"Received Window Closing event from Stride.  Stopping Application.");
+            HostApplicationLifetime.StopApplication();
+        }
+
+        #endregion
+
+#if TRIAGE // TODO - was WIP?
+        ConcurrentDictionary<SceneInstance, StrideServiceRegistry> sceneRegistries = new ConcurrentDictionary<SceneInstance, StrideServiceRegistry>();
+
+        //public IServiceProvider GetServiceProvider(SceneInstance sceneInstance = null)
+        //{
+        //    if (sceneInstance != null)
+        //    {
+        //        // FUTURE: different IServiceProvider for non-default sceneInstance
+        //        throw new NotImplementedException();
+        //    }
+
+        //    sceneRegistries.GetOrAdd(sceneInstance, s => new StrideSceneRegistry(s));
+        //    Game.SceneSystem.SceneInstance.Services
+
+        //}
+#endif
 
 
+#if UNUSED
         #region DataContext
 
         public object DataContext
@@ -42,65 +147,6 @@ namespace LionFire.Stride3D
         private object dataContext;
 
         #endregion
-
-        public IHostApplicationLifetime HostApplicationLifetime { get; }
-        public ILogger<StrideGameService> Logger { get; }
-
-        ConcurrentDictionary<SceneInstance, StrideServiceRegistry> sceneRegistries = new ConcurrentDictionary<SceneInstance, StrideServiceRegistry>();
-
-        //public IServiceProvider GetServiceProvider(SceneInstance sceneInstance = null)
-        //{
-        //    if (sceneInstance != null)
-        //    {
-        //        // FUTURE: different IServiceProvider for non-default sceneInstance
-        //        throw new NotImplementedException();
-        //    }
-
-        //    sceneRegistries.GetOrAdd(sceneInstance, s => new StrideSceneRegistry(s));
-        //    Game.SceneSystem.SceneInstance.Services
-
-        //}
-
-        public StrideGameService(IHostApplicationLifetime hostApplicationLifetime, ILogger<StrideGameService> logger)
-        {
-            HostApplicationLifetime = hostApplicationLifetime;
-            Logger = logger;
-        }
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            runTask = Task.Run(() =>
-            {
-                using (game = new Game())
-                {
-                    Task.Run(async () =>
-                    {
-                        while (game?.Window == null)
-                        {
-                            Logger.LogDebug("Waiting for Game.Window to appear...");
-                            await Task.Delay(200);
-                        }
-                        Logger.LogDebug("Waiting for Game window to appear...done.");
-                        game.Window.Closing += Window_Closing;
-                    });
-                    game.Run();
-                }
-            });
-            return Task.CompletedTask;
-        }
-
-        private void Window_Closing(object sender, EventArgs e)
-        {
-            Logger.LogInformation($"Received Window Closing event from Stride.  Stopping Application.");
-            HostApplicationLifetime.StopApplication();
-        }
-
-        public async Task StopAsync(CancellationToken cancellationToken)
-        {
-            game.Exit();
-            while (game.IsRunning)
-            {
-                await Task.Delay(100).ConfigureAwait(false);
-            }
-        }
+#endif
     }
 }
