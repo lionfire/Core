924181791e1ec841dec4567294f63567f6a2e1de
Jared Thirsk <ja+commits@thirsk.ca>
2024-03-13 17:32:26 -0600

    WIP - Massive DateTime to DateTimeOffset conversion

---
+++ src/LionFire.Base/DateTime/DateTimeOffsetX.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LionFire.ExtensionMethods;
+
+public static class DateTimeOffsetX
+{
+    /// <summary>
+    /// Assumes Offset of Zero
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static DateTimeOffset ToDateTimeOffset(this Int64 value) => new DateTimeOffset(DateTime.FromBinary(value), TimeSpan.Zero);
+    public static DateTimeOffset ToDateTimeOffset(this Int64 value, TimeSpan timeSpan) => new DateTimeOffset(DateTime.FromBinary(value), timeSpan);
+    public static long ToBinaryWithoutOffset(this DateTimeOffset dateTimeOffset)
+        => dateTimeOffset.DateTime.ToBinary();
+}
--- src/LionFire.Base/System/DateRangeUtils.cs
+++ src/LionFire.Base/System/DateRangeUtils.cs
@@ -13,6 +13,24 @@ public class DateRangeUtils
         }
         return (start, endExclusive);
     }
+    public static (DateTimeOffset start, DateTimeOffset end) GetMonths(DateTimeOffset date, int months = 1)
+    {
+        var start = new DateTimeOffset(date.Year, date.Month, 1, 0, 0, 0, TimeSpan.Zero);
+        var endExclusive = start;
+        for (int i = 0; i < months; i++)
+        {
+            endExclusive = endExclusive + TimeSpan.FromDays(31);
+            endExclusive = new DateTime(endExclusive.Year, endExclusive.Month, 1, 0, 0, 0, DateTimeKind.Utc);
+        }
+        return (start, endExclusive);
+    }
+
+    public static (DateTimeOffset start, DateTimeOffset endExclusive) GetDays(DateTimeOffset date, int days = 1)
+    {
+        var start = new DateTimeOffset(date.Year, date.Month, date.Day, 0, 0, 0, TimeSpan.Zero);
+        var endExclusive = start + TimeSpan.FromDays(days);
+        return (start, endExclusive);
+    }
     public static (DateTime start, DateTime endExclusive) GetDays(DateTime date, int days = 1)
     {
         var start = new DateTime(date.Year, date.Month, date.Day, 0, 0, 0, DateTimeKind.Utc);
@@ -24,6 +42,12 @@ public class DateRangeUtils
     {
         var start = new DateTime(date.Year, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         var endExclusive = new DateTime(date.Year + 1, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        return (start, endExclusive);
+    }
+    public static (DateTimeOffset start, DateTimeOffset endExclusive) GetYear(DateTimeOffset date)
+    {
+        var start = new DateTimeOffset(date.Year, 1, 1, 0, 0, 0, TimeSpan.Zero);
+        var endExclusive = new DateTimeOffset(date.Year + 1, 1, 1, 0, 0, 0, TimeSpan.Zero);
         return (start, endExclusive);
     }
 }
