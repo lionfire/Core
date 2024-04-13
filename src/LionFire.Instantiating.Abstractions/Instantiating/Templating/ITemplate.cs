9b94f9ede082750c4fa5b3a258ad7dc47799537a
Jared Thirsk <ja+commits@thirsk.ca>
2024-04-13 15:08:35 -0600

    reorg, Inidicator unit test working again

--- src/LionFire.Instantiating.Abstractions/Instantiating/Templating/ITemplate.cs
+++ src/LionFire.Instantiating.Abstractions/Instantiating/Templating/ITemplate.cs
@@ -3,29 +3,28 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Threading.Tasks;
 
-namespace LionFire.Instantiating
+namespace LionFire.Instantiating;
+
+public interface ITemplate { }
+
+public interface ITemplateEx
 {
-    public interface ITemplate { }
+    Type InstantiationType { get; }
+}
 
-    public interface ITemplateEx
-    {
-        Type InstantiationType { get; }
-    }
+// UNUSED. REVIEW - avoid this?
+public interface IInstantiatingTemplate // RENAME: IGenerator
+{
+    object Instantiate();
+}
 
-    // UNUSED. REVIEW - avoid this?
-    public interface IInstantiatingTemplate // RENAME: IGenerator
-    {
-        object Instantiate();
-    }
+public interface ITemplate<TInstance> : ITemplate, IForInstancesOf<TInstance>
+//where T : new()
+{
+}
 
-    public interface ITemplate<TInstance> : ITemplate, IForInstancesOf<TInstance>
-    //where T : new()
-    {
-    }
-
-    public interface ITemplate<TTemplate, TInstance, TParameters> : ITemplate, IForInstancesOf<TInstance>
-        where TTemplate : ITemplate
-        where TParameters : ITemplateParameters<TTemplate>
-    {
-    }
+public interface ITemplate<TTemplate, TInstance, TParameters> : ITemplate, IForInstancesOf<TInstance>
+    where TTemplate : ITemplate
+    where TParameters : ITemplateParameters<TTemplate>
+{
 }
