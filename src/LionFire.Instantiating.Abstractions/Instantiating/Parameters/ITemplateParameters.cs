3d01325fa39852cee3236d0e46fabc7c34935b9b
Jared Thirsk <ja+commits@thirsk.ca>
2024-04-21 17:15:28 -0600

    wip: template clarifications

--- src/LionFire.Instantiating.Abstractions/Instantiating/Parameters/ITemplateParameters.cs
+++ src/LionFire.Instantiating.Abstractions/Instantiating/Parameters/ITemplateParameters.cs
@@ -2,33 +2,35 @@
 using LionFire.Referencing;
 using LionFire.Structures;
 
-namespace LionFire.Instantiating
+namespace LionFire.Instantiating;
+
+// TODO: Rename this to IReferencingTemplateParameters
+
+// REVIEW TOCOMMENT: Why does this have a key?  Is the key just the Template's Reference?
+public interface ITemplateParameters : IKeyed<string>, IHasRTemplate
+    //, ITemplateOverlayable // This breaks AssetInstantiation
 {
-    public interface ITemplateParameters : IKeyed<string>, IHasRTemplate
-        //, ITemplateOverlayable // This breaks AssetInstantiation
-    {
-        //ParameterOverlayMode OverlayMode { get; }
-        //object OverlayParent { get; set; }
-        //IReadHandleBase<ITemplate> RTemplate { get;  }
-        //IReadHandle<ITemplateAsset> TemplateAsset { get; set; }
-    }
+    //ParameterOverlayMode OverlayMode { get; }
+    //object OverlayParent { get; set; }
+    //IReadHandleBase<ITemplate> RTemplate { get;  }
+    //IReadHandle<ITemplateAsset> TemplateAsset { get; set; }
+}
 
-    public interface ITemplateParameters<TTemplate> : ITemplateParameters
-       where TTemplate : ITemplate
-    {
-        new IReadHandleBase<TTemplate> RTemplate { get; set; }
-    }
-    public interface ITemplateHandleParameters<TTemplate, TTemplateHandle> : ITemplateParameters
-      where TTemplate : ITemplate
-    {
-        new TTemplateHandle RTemplate { get; set; }
-    }
+public interface ITemplateParameters<TTemplate> : ITemplateParameters
+   where TTemplate : ITemplate
+{
+    new IReadHandleBase<TTemplate> RTemplate { get; set; }
+}
+public interface ITemplateHandleParameters<TTemplate, TTemplateHandle> : ITemplateParameters
+  where TTemplate : ITemplate
+{
+    new TTemplateHandle RTemplate { get; set; }
+}
 
-    public interface ITemplateParameters<TemplateType, TInstance>
-        : IInstantiation<TemplateType>
-        , ITemplateParameters
-        where TemplateType : ITemplate<TInstance>
-        //where ParametersType : ITemplateParameters //, new()
-    {
-    }
+public interface ITemplateParameters<TemplateType, TInstance>
+    : IInstantiation<TemplateType>
+    , ITemplateParameters
+    where TemplateType : ITemplate<TInstance>
+    //where ParametersType : ITemplateParameters //, new()
+{
 }
--- src/LionFire.Instantiating.Abstractions/Instantiating/Templating/ITemplateInstance.cs
+++ src/LionFire.Instantiating.Abstractions/Instantiating/Templating/ITemplateInstance.cs
@@ -4,46 +4,53 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
-namespace LionFire.Instantiating
+namespace LionFire.Instantiating;
+
+/// <summary>
+/// REVIEW: REFACTOR - If this is just a marker interface, drop it as a constraint on generic Template classes
+/// </summary>
+public interface ITemplateInstance
 {
-    /// <summary>
-    /// REVIEW: REFACTOR - If this is just a marker interface, drop it as a constraint on generic Template classes
-    /// </summary>
-    public interface ITemplateInstance
-    {
-        // Overkill?  If you want to get the template, search for ITemplateInstance<>'s -- there should only be one, or at least one that has a null value in Template.
-        //ITemplate Template { get; set; }
-    }
+    // Overkill?  If you want to get the template, search for ITemplateInstance<>'s -- there should only be one, or at least one that has a null value in Template.
+    //ITemplate Template { get; set; }
+}
 
-    public interface IParameterizedTemplateInstance
-    {
-        ITemplateParameters Parameters { get; set; }
-    }
+public interface IParameterizedTemplateInstance
+{
+    ITemplateParameters Parameters { get; set; }
+}
+public interface IParameterizedTemplateInstance<out TParameters>
+{
+    ITemplateParameters Parameters { get; set; }
+}
+public interface IParameterizedTemplateInstance<out TParameters, out TTemplate>
+{
+    ITemplateParameters<TTemplate> Parameters { get; set; }
+}
 
-    public interface ITemplateInstance<TTemplate> : ITemplateInstance, IInstanceFor<TTemplate>
-        where TTemplate : ITemplate
-    {
-         new TTemplate Template { get; set; }
-    }
+public interface ITemplateInstance<TTemplate> : ITemplateInstance, IInstanceFor<TTemplate>
+    where TTemplate : ITemplate
+{
+     new TTemplate Template { get; set; }
+}
 
-    public static class ITemplateInstanceExtensions
+public static class ITemplateInstanceExtensions
+{
+    private static PropertyInfo GetTemplateProperty(ITemplateInstance obj)
     {
-        private static PropertyInfo GetTemplateProperty(ITemplateInstance obj)
+        foreach (var pi in obj.GetType().GetProperties().Where(p => p.Name == "Template"))
         {
-            foreach (var pi in obj.GetType().GetProperties().Where(p => p.Name == "Template"))
-            {
-                if (pi.PropertyType == typeof(ITemplate) || typeof(ITemplate).IsAssignableFrom(pi.PropertyType)) return pi;
-            }
-            throw new Exception("Template property that returns ITemplate or derived type not found on object of type " + obj.GetType().FullName);
+            if (pi.PropertyType == typeof(ITemplate) || typeof(ITemplate).IsAssignableFrom(pi.PropertyType)) return pi;
         }
-        public static ITemplate GetTemplate(this ITemplateInstance templateInstance)
-        {
-            if (templateInstance == null) return null;
-            return (ITemplate)GetTemplateProperty(templateInstance).GetValue(templateInstance);
-        }
-        public static void SetTemplate(this ITemplateInstance templateInstance, ITemplate template)
-        {
-            GetTemplateProperty(templateInstance).SetValue(templateInstance, template);
-        }
+        throw new Exception("Template property that returns ITemplate or derived type not found on object of type " + obj.GetType().FullName);
+    }
+    public static ITemplate GetTemplate(this ITemplateInstance templateInstance)
+    {
+        if (templateInstance == null) return null;
+        return (ITemplate)GetTemplateProperty(templateInstance).GetValue(templateInstance);
+    }
+    public static void SetTemplate(this ITemplateInstance templateInstance, ITemplate template)
+    {
+        GetTemplateProperty(templateInstance).SetValue(templateInstance, template);
     }
 }
