b54bdd0e0d208ce2e0cc96e41eecac09bf5dec48
Jared Thirsk <ja@thirsk.ca>
2024-05-30 12:08:04 -0600

    WIP: start reorg toward DataFlow namespace.  Progress on Input binding.

--- src/LionFire.Structures/Events/ICancelable.cs
+++ src/LionFire.Structures/Events/ICancelable.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace LionFire.Events
+namespace LionFire.Events;
+
+public interface ICancelable // RENAME ICancellable
 {
-    public interface ICancelable
-    {
-        /// <summary>
-        /// Request that the operation be canceled, optionally providing a reason for the cancelation.
-        /// </summary>
-        /// <param name="reason">A string or object containing info about why the cancelation is requested.</param>
-        void Cancel(object reason = null);
-    }
+    /// <summary>
+    /// Request that the operation be canceled, optionally providing a reason for the cancelation.
+    /// </summary>
+    /// <param name="reason">A string or object containing info about why the cancelation is requested.</param>
+    void Cancel(object reason = null);
+}
+
+public interface IAsyncCancellable
+{
+    Task Cancel(CancellationToken cancellationToken = default);
 }
--- src/LionFire.Structures/Execution/IRunnable.cs
+++ src/LionFire.Structures/Execution/IRunnable.cs
@@ -5,7 +5,7 @@ namespace LionFire.Execution;
 public interface IRunnable : IStartable
 {
     //CancellationToken Terminated { get; }
-    Task<bool> RunTask { get; }
+    Task RunTask { get; }
 }
 
 public static class IRunnableX
@@ -13,7 +13,7 @@ public static class IRunnableX
     public static async Task Run(this IRunnable @this, CancellationToken cancellationToken = default)
     {
         await @this.StartAsync(cancellationToken);
-        await @this.RunTask;
+        await @this.RunTask; // TODO: use cancellationToken 
         //await @this.Terminated;
     }
 }
