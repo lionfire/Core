ceb6454c80260ee79201c8e2f406985fbfa2e40b
Jared Thirsk <ja+commits@thirsk.ca>
2024-06-30 20:50:23 -0600

    Metaverse server now starts up.  (Copied from Universe Silo).  - Fixed missing ref dlls

---
+++ src/LionFire.AspNetCore/Auth/InitialApplicationState.cs
@@ -0,0 +1,9 @@
+﻿
+namespace LionFire.AspNetCore;
+
+// REVIEW - is this needed?
+public class InitialApplicationState
+{
+    public string? AccessToken { get; set; }
+    public string? RefreshToken { get; set; }
+}
---
+++ src/LionFire.AspNetCore/Auth/TokenProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IdentityModel.Tokens.Jwt;
+using System.Threading.Tasks;
+
+namespace LionFire.AspNetCore;
+
+// Based on: https://docs.microsoft.com/en-us/aspnet/core/blazor/security/server/additional-scenarios?view=aspnetcore-6.0
+public class TokenProvider
+{
+    public string? AccessToken { get; set; }
+    public string? RefreshToken { get; set; }
+
+    public JwtSecurityToken? Jwt
+    {
+        get
+        {
+            if (jwt == null && AccessToken != null)
+            {
+                jwt = new JwtSecurityTokenHandler().ReadJwtToken(AccessToken);
+            }
+            return jwt;
+        }
+    }
+    private JwtSecurityToken? jwt;
+ 
+}
