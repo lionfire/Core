26d91ab3b08105f5213f19d28ce953ba43f289fa
Jared Thirsk <ja+commits@thirsk.ca>
2024-12-16 17:43:25 -0700

    DynamicHostedService, OnStart extension for host builders

---
+++ src/LionFire.Hosting/Hosting/HostedServices/DynamicHostedService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+
+namespace LionFire.Hosting;
+
+public class DynamicHostedService : IHostedService
+{
+    public IServiceProvider ServiceProvider { get; }
+
+    public DynamicHostedService(IServiceProvider serviceProvider)
+    {
+        ServiceProvider = serviceProvider;
+    }
+
+    public Func<IServiceProvider, CancellationToken, ValueTask>? OnStart { get; set; }
+    public Func<IServiceProvider, CancellationToken, ValueTask>? OnStop { get; set; }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        if (OnStart != null) await OnStart(ServiceProvider, cancellationToken);
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        if (OnStop != null) await OnStop(ServiceProvider, cancellationToken);
+    }
+}
---
+++ src/LionFire.Hosting/Hosting/HostedServices/HostedServiceHostBuilderX.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Threading;
+
+namespace LionFire.Hosting;
+
+public static class HostedServiceHostBuilderX
+{
+    public static IHostBuilder UseHostedService<T>(this IHostBuilder hostBuilder)
+        where T : class, IHostedService
+    {
+        return hostBuilder.ConfigureServices((context, services) =>
+            services.AddHostedService<T>());
+    }
+    public static IHostApplicationBuilder UseHostedService<T>(this IHostApplicationBuilder hostApplicationBuilder)
+        where T : class, IHostedService
+    {
+        hostApplicationBuilder.Services.AddHostedService<T>();
+        return hostApplicationBuilder;
+    }
+
+    #region DynamicHostedService helpers
+
+    public static IServiceCollection OnStart(this IServiceCollection services, Func<IServiceProvider, CancellationToken, ValueTask> action)
+        => services.AddHostedService(sp => new DynamicHostedService(sp)
+        {
+            OnStart = action,
+        });
+
+    public static ILionFireHostBuilder OnStart(this ILionFireHostBuilder lionFireHostBuilder, Func<IServiceProvider, CancellationToken, ValueTask> action)
+        => lionFireHostBuilder.ConfigureServices(s => s.OnStart(action));
+    public static ILionFireHostBuilder OnStart(this ILionFireHostBuilder lionFireHostBuilder, Action<IServiceProvider> action)
+        => lionFireHostBuilder.ConfigureServices(s => s.OnStart((sp, ct) => { action(sp); return ValueTask.CompletedTask; }));
+
+    #endregion
+
+}
