8b740a35b080e1cbd0f39f7e9f5431337fbfa545
Jared Thirsk <ja+commits@thirsk.ca>
2025-04-21 13:30:02 -0600

    docker detection

---
+++ src/LionFire.Hosting/Containerization/ContainerizationHostingX.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LionFire.Hosting;
+
+public static class ContainerizationHostingX
+{
+    public static bool IsInDockerContainer
+        => File.Exists("/.dockerenv")
+        || File.Exists("/proc/self/cgroup") && File.ReadAllText("/proc/self/cgroup").Contains("docker");
+
+    public static IHostApplicationBuilder TryAddConfigForDocker(this IHostApplicationBuilder hostBuilder, bool reloadOnChange = true, bool force = false, string confDir = "/app/conf")
+    {
+        if (!force && !IsInDockerContainer) return hostBuilder;
+
+        var configFile = Path.Combine(confDir, "appsettings.json");
+        Log.Get(typeof(ReleaseChannelsHostingX).FullName).LogInformation($"Adding configuration source for docker: {configFile}");
+        hostBuilder.Configuration.AddJsonFile(configFile, optional: true, reloadOnChange);
+        return hostBuilder;
+    }
+
+}
